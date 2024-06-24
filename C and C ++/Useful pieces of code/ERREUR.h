9bfad20a4023059e7feef5865b170b789559ff83
SPTApyo <laroussinabil032@gmail.com>
2024-06-24 21:59:13 +0200

    New pieces of useful code

---
+++ C and C ++/Useful pieces of code/ERREUR.h
@@ -0,0 +1,16 @@
+#ifndef ERREUR_H_INCLUDED
+#define ERREUR_H_INCLUDED
+
+void ERREUR(void)
+{
+    Color(15, 0);
+    printf("[");
+    Color(12,0);
+    printf("ERREUR");
+    Color(15, 0);
+    printf("]");
+    printf(">");
+}
+
+#endif // ERREUR_H_INCLUDED
+
