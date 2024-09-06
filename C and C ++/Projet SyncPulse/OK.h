479c0a969b785cd47a052526b76ec2a272371371
SPTApyo <laroussinabil032@gmail.com>
2024-09-06 19:07:05 +0200

    New project

---
+++ C and C ++/Projet SyncPulse/OK.h
@@ -0,0 +1,16 @@
+#ifndef OK_H_INCLUDED
+#define OK_H_INCLUDED
+
+void OK(void)
+{
+    Color(15, 0);
+    printf("[");
+    Color(10,0);
+    printf("OK");
+    Color(15, 0);
+    printf("]");
+    printf(">");
+}
+
+
+#endif // OK_H_INCLUDED
---
+++ C and C ++/Projet SyncPulse/WAIT.h
@@ -0,0 +1,16 @@
+#ifndef WAIT_H_INCLUDED
+#define WAIT_H_INCLUDED
+
+void WAIT(void)
+{
+    Color(15, 0);
+    printf("[");
+    Color(9,0);
+    printf("WAIT");
+    Color(15, 0);
+    printf("]");
+    printf(">");
+}
+
+#endif // WAIT_H_INCLUDED
+
