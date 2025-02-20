bda441090e7e042bb054f9365935df22fab42bb3
SPTApyo <laroussinabil032@gmail.com>
2025-02-20 20:09:59 +0100

    Delete Projet Autre/Raspberry Pi project/Script/docker_update.sh

--- Projet Autre/Raspberry Pi project/Script/docker_update.sh
+++
@@ -1,6 +0,0 @@
-docker stop $(docker ps)
-docker rmi -f $(docker images -a -q)
-docker rm DiscordBot
-docker pull apyo37/iut-bot:latest
-docker run --name DiscordBot -e DISCORD_TOKEN=XXXX -v ./DockerBot/log:/home/Grace/DockerBot/log -v ./DockerBot/data:/home/Grace/DockerBot/data -p 49160:8080 -d apyo37/iut-bot
-docker logs -f DiscordBot
