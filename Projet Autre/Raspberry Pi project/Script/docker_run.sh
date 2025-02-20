c8374ce3bdc42bd78725f803f9324121a93afcba
SPT_Apyo <laroussinabil032@gmail.com>
2025-02-20 20:02:42 +0100

    Remove sensitive information from Docker run commands in scripts

--- Projet Autre/Raspberry Pi project/Script/docker_run.sh
+++ Projet Autre/Raspberry Pi project/Script/docker_run.sh
@@ -1,4 +1,4 @@
 docker stop $(docker ps)
 docker rm DiscordBot
-docker run --name DiscordBot -e DISCORD_TOKEN=MTI4NzQ3MjgzNDIyNjAzMjY5MQ.GdApoM.oLv4graIDkum91yHCgqpJekeBnqSgQR_xZAngQ -v ./DockerBot/log:/home/Grace/DockerBot/log -v ./DockerBot/data:/home/Grace/DockerBot/data -p 49160:8080 -d apyo37/iut-bot
+docker run --name DiscordBot -e DISCORD_TOKEN=XXXX -v ./DockerBot/log:/home/Grace/DockerBot/log -v ./DockerBot/data:/home/Grace/DockerBot/data -p 49160:8080 -d apyo37/iut-bot
 docker logs -f DiscordBot
